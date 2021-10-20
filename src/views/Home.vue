91caf82ea87be7d94409e08bd1b9a5aa42861f1a
ntnrdn1 <nathan.neves@aluno.usj.edu.br>
2021-10-20 16:13:53 -0300

    configuração de leitura do gps

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,25 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
+   <ion-header>
+     <ion-toolbar>
+       <ion-title>Por onde andei?</ion-title>
+     </ion-toolbar>
+   </ion-header>
+
+   <ion-content>
+     <div class="container">
+       <h2>Coordenadas</h2>
+       <h3>{{ latitude }}</h3>
+       <h3>{{ longitude }}</h3>
+     </div>
+   </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import { Geolocation } from '@capacitor/geolocation';
 
 export default defineComponent({
   name: 'Home',
@@ -33,36 +29,32 @@ export default defineComponent({
     IonPage,
     IonTitle,
     IonToolbar
+  },
+  data() {
+    return { 
+      latitude: 0,
+      longitude: 0,
+    }
+  },
+  ionViewDidEnter() {
+    this.printCurrentPosition();
+  },
+  methods: {
+    printCurrentPosition: async function() {
+      const coordinates = await Geolocation.getCurrentPosition();
+      console.log('Current position:', coordinates);
+
+      this.latitude = coordinates.coords.latitude;
+      this.longitude = coordinates.coords.longitude;
+    }
   }
 });
 </script>
 
 <style scoped>
-#container {
+
+.container {
   text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
 </style>
